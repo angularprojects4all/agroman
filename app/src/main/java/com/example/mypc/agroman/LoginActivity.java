da6ccdfb89eb3de5a6012d18b7976d1e0a61c23c
sukumar4open <36978810+sukumar4open@users.noreply.github.com>
2018-03-02 10:25:22 +0530

    Update LoginActivity.java

--- app/src/main/java/com/example/mypc/agroman/LoginActivity.java
+++ app/src/main/java/com/example/mypc/agroman/LoginActivity.java
@@ -10,7 +10,7 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
-//import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.Auth;
 import android.content.CursorLoader;
 import android.content.Loader;
 import android.database.Cursor;
